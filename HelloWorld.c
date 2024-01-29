16acda8d3bb8e366994d41b6516f282b0e80c5ab
David Smith <djsmith611@me.com>
2024-01-28 19:58:29 -0700

    added examples of data types and variables

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,4 +1,5 @@
-#include <stdio.h> //standard input/output. header
+#include <stdio.h>      //standard input/output. header
+#include <stdbool.h>    //to include booleans
 int main(){
    /*   escape sequence = character combination consisting of a backslash \
                             followed byyy a letter or combination of digits.
@@ -32,8 +33,40 @@ int main(){
    printf("Your average grade is %c\n",grade);  //%c character
    printf("your gpa is %f\n",gpa);              //%f float
 
-   float c = 3.141592;          //4 bytes (32 bits of precision) 6-7 digits %f
-   double d = 3.141592653589793;//8 bytes (64 bits of precision) 15-16 digits %lf
+   float c = 3.141592653589793;          //4 bytes (32 bits of precision) 6-7 digits %f
+   double d = 3.141592653589793;//8 bytes (64 bits of precision) 15-16 digits %lf (long float)
+
+   printf("%0.15f\n",c); //0.15 will push past character limit
+   printf("%0.15lf\n",d);//double is more accurate and retains the correct numbers
+
+   bool e = true;   // 1 byte (true or false) %d
+
+   printf("\'%d\' means true!!\n",e); //1 for true 0 for false
+
+   char f = 100;    // 1 byte (-128 to 127) %d or %c 
+                    //can use ASCII table to convert this number (decimal) to a character representation
+   printf("The \"Decimal\" value is \'%d\'\n",f);
+   printf("The corresonding character is \'%c\'\n",f);
+
+   unsigned char g = 255;   // 1 byte (0 to 255) %d or %c
+   printf("%d\n",g);
+
+   short int h = 32767;         // 2 bytes (-32,768 to 32,767) %d (dont need int)
+   unsigned short int i = 65535;// 2 bytes (0 to 65,535) %d (dont need int)
+   printf("%d\n",h);
+   printf("%d\n",i);
+
+   int j = 2147483647;          //4 bytes (-2,147,483,648 to 2,147,483,647) %d
+   unsigned int k = 4294967295; //4 bytes (0 to 4,294,967,295) %u
+   printf("%d\n",j);
+   printf("%u\n",k);
+
+   long long int l = 9223372036854775807;           // 8 bytes (-9 quintillion to 9 quintillion) %lld
+   unsigned long long int m = 18446744073709551615; //8 bytes (0 to 18 quintillion) %llu
+   printf("%lld\n",l);
+   printf("%llu\n",m);
+
+
 
 
    return 0;
